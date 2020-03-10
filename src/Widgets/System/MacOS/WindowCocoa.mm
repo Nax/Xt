397a7b9af60b84e14dc89506599161e8e84182d6
Maxime Bacoux <max@bacoux.com>
2020-03-10 13:27:32 +0100

    Very basic windows on macOS

--- src/Widgets/System/MacOS/WindowCocoa.mm
+++ src/Widgets/System/MacOS/WindowCocoa.mm
@@ -4,7 +4,8 @@ using namespace xt;
 
 WindowCocoa::WindowCocoa()
 {
-
+    _window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 640, 480) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:YES];
+    [_window makeKeyAndOrderFront:nil];
 }
 
 WindowCocoa::~WindowCocoa()
